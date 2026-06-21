6bbc579b5103068afce75cc381eff48658fb8af5
Henry Reid <henryreid05@gmail.com>
2026-06-21 18:44:17 +0100

    updated nvim

--- nvim/lua/henry/plugins/completion.lua
+++ nvim/lua/henry/plugins/completion.lua
@@ -1,42 +1,41 @@
 return {
-  {
-    "saghen/blink.cmp",
-    version = "*",
-    dependencies = {
-      "rafamadriz/friendly-snippets", -- large snippet library
-    },
-    opts = {
-      keymap = {
-        preset = "default",
-        -- override individual keys if you want:
-        -- ["<C-y>"] = { "accept", "fallback" },
-        -- ["<C-e>"] = { "cancel", "fallback" },
-      },
-
-      appearance = {
-        use_nvim_cmp_as_default = false,
-        nerd_font_variant = "mono",
-      },
-
-      sources = {
-        default = { "lsp", "path", "snippets", "buffer" },
-      },
-
-      completion = {
-        documentation = {
-          auto_show = true,
-          auto_show_delay_ms = 200,
+    {
+        "saghen/blink.cmp",
+        version = "*",
+        dependencies = {
+            "rafamadriz/friendly-snippets", -- large snippet library
         },
-        menu = {
-          draw = {
-            treesitter = { "lsp" }, -- use treesitter highlighting in menu
-          },
-        },
-      },
+        opts = {
+            keymap = {
+                preset = "default",
+                -- Override or add <Tab> here:
+                ["<Tab>"] = { "select_and_accept", "fallback" },
+            },
 
-      signature = {
-        enabled = true, -- show function signature while typing args
-      },
+            appearance = {
+                use_nvim_cmp_as_default = false,
+                nerd_font_variant = "mono",
+            },
+
+            sources = {
+                default = { "lsp", "path", "snippets", "buffer" },
+            },
+
+            completion = {
+                documentation = {
+                    auto_show = true,
+                    auto_show_delay_ms = 200,
+                },
+                menu = {
+                    draw = {
+                        treesitter = { "lsp" }, -- use treesitter highlighting in menu
+                    },
+                },
+            },
+
+            signature = {
+                enabled = true, -- show function signature while typing args
+            },
+        },
     },
-  },
 }
