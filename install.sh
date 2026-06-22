76ca6cba25c72507f45c8bfca2f72fae0361fcf7
Henry Reid <henryreid05@gmail.com>
2026-06-22 23:39:41 +0100

    updated bash script for automated installation for tmux and nvim

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+CONFIG_HOME="$HOME/.config"
+
+# nvim
+rm -rf "$CONFIG_HOME/nvim"
+cp -r nvim "$CONFIG_HOME/"
+echo "nvim installation complete"
+
+# tmux
+rm -rf "$CONFIG_HOME/tmux"
+cp -r tmux "$CONFIG_HOME/"
+if [ ! -d "$CONFIG_HOME/tmux/plugins/tpm" ]; then
+    git clone https://github.com/tmux-plugins/tpm "$CONFIG_HOME/tmux/plugins"
+fi
+tmux source "$CONFIG_HOME/tmux/tmux.conf"
+echo "tmux installation complete"
