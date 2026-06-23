3a34d59287a16163827d4833e6a4edd4cd537d41
Henry Reid <henryreid05@gmail.com>
2026-06-23 16:27:15 +0100

    fixed bash script

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ echo "nvim installation complete"
 rm -rf "$CONFIG_HOME/tmux"
 cp -r tmux "$CONFIG_HOME/"
 if [ ! -d "$CONFIG_HOME/tmux/plugins/tpm" ]; then
-    git clone https://github.com/tmux-plugins/tpm "$CONFIG_HOME/tmux/plugins"
+    git clone https://github.com/tmux-plugins/tpm "$CONFIG_HOME/tmux/plugins/tpm"
 fi
 tmux source "$CONFIG_HOME/tmux/tmux.conf"
 echo "tmux installation complete"
